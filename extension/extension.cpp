471c87aaf6e05388ca7a18685cb9b52a37ce643f
Maxime Leroy <19607336+maxime1907@users.noreply.github.com>
2025-02-03 19:20:05 +0100

    feat: forward begin auth session result (#32)

--- extension/extension.cpp
+++ extension/extension.cpp
@@ -34,6 +34,7 @@ IGameConfig *g_pGameConf = NULL;
 
 IForward *g_pConnectForward = NULL;
 IForward *g_pOnValidateAuthTicketResponse = NULL;
+IForward *g_pOnBeginAuthSessionResult = NULL;
 
 class IClient;
 class CBaseClient;
@@ -229,7 +230,16 @@ EBeginAuthSessionResult BeginAuthSession(const void *pAuthTicket, int cbAuthTick
 	} u;
 	u.mfp.Init(func);
 
-	return (EBeginAuthSessionResult)(reinterpret_cast<VFuncEmptyClass*>(g_pSteam3Server->m_pSteamGameServer)->*u.mfpnew)(pAuthTicket, cbAuthTicket, steamID);
+	EBeginAuthSessionResult result = (EBeginAuthSessionResult)(reinterpret_cast<VFuncEmptyClass*>(g_pSteam3Server->m_pSteamGameServer)->*u.mfpnew)(pAuthTicket, cbAuthTicket, steamID);
+
+	g_pOnBeginAuthSessionResult->PushString(steamID.Render());
+	g_pOnBeginAuthSessionResult->PushCell(result);
+
+	cell_t retVal = result;
+	g_pOnBeginAuthSessionResult->Execute(&retVal);
+	result = (EBeginAuthSessionResult)retVal;
+
+	return result;
 }
 
 void EndAuthSession(CSteamID steamID)
@@ -453,6 +463,8 @@ bool Connect::SDK_OnLoad(char *error, size_t maxlen, bool late)
 
 	g_pOnValidateAuthTicketResponse = g_pForwards->CreateForward("OnValidateAuthTicketResponse", ET_Ignore, 2, NULL, Param_String, Param_Cell);
 
+	g_pOnBeginAuthSessionResult = g_pForwards->CreateForward("OnBeginAuthSessionResult", ET_Ignore, 2, NULL, Param_String, Param_Cell);
+
 	g_pConnectForward = g_pForwards->CreateForward("OnClientPreConnectEx", ET_LowEvent, 5, NULL, Param_String, Param_String, Param_String, Param_String, Param_String);
 
 	return true;
@@ -467,10 +479,11 @@ bool Connect::SDK_OnMetamodLoad(ISmmAPI *ismm, char *error, size_t maxlen, bool 
 	return true;
 }
 
-void Connect::SDK_OnUnload() 
+void Connect::SDK_OnUnload()
 {
 	g_pForwards->ReleaseForward(g_pConnectForward);
 	g_pForwards->ReleaseForward(g_pOnValidateAuthTicketResponse);
+	g_pForwards->ReleaseForward(g_pOnBeginAuthSessionResult);
 
 	gameconfs->CloseGameConfigFile(g_pGameConf);
 }
